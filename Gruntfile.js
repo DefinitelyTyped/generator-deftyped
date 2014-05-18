f8e1d7c6cfd3c5ea8e43189e02d69c04e9ed5009
Diullei Gomes <diullei@gmail.com>
2014-05-18 00:16:02 -0300

    add tslint, jshint to grunt; fix Gruntfile.js jshint warnings

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,14 @@
 module.exports = function(grunt) {
-    "use strict";
+    'use strict';
 
     grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-tslint');
 
     grunt.initConfig({
         ts: {
             build: {
-                src: ["app/**/*.ts"],
+                src: ['app/**/*.ts'],
                 options: {
                     target: 'es3',
                     module: 'commonjs',
@@ -15,8 +17,22 @@ module.exports = function(grunt) {
                     removeComments: true
                 },
             }
+        },
+        jshint: {
+            all: ['Gruntfile.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        tslint: {
+            options: {
+                configuration: grunt.file.readJSON('tslintrc.json')
+            },
+            all: {
+                src: ['app/**/*.ts']
+            }
         }
     });
 
-    grunt.registerTask("default", ["ts:build"]);
+    grunt.registerTask('default', ['jshint:all', 'tslint:all', 'ts:build']);
 };
