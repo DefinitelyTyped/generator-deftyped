e9d1ddb2873a3166b5e8ed2f2c6a7f5db082c8d5
Diullei Gomes <diullei@gmail.com>
2014-05-22 12:46:02 -0300

    ...

---
+++ app/validatior.ts
@@ -0,0 +1,12 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+import defHeader = require('../definition-header/src/index');
+import model = require('../definition-header/src/model');
+
+//var validHeader = model.header
+
+class HeaderValidator {
+    public static validateTypingName() {
+        return null;
+    }
+}
